98c8548ca4bdda5f9f81bff888253408fee1af17
Justin White <kyjus25@gmail.com>
2019-12-26 12:55:32 -0600

    Only 1 instance of bedrive. Ever.

--- run.sh
+++ run.sh
@@ -1,10 +1,10 @@
 if [ "$(docker ps -q --filter ancestor=bedrive)" != "" ]; then
     docker stop $(docker ps -q --filter ancestor=bedrive)
-    docker rm $(docker ps -q --filter ancestor=bedrive)
+    docker start bedrive
+else
+    docker build -t bedrive .
+    docker run -it -d -p 80:80 -p 3306:3306 -e MYSQL_ROOT_PASSWORD=password -v $(pwd)/bedrive:/var/www/localhost/htdocs/ --name bedrive bedrive
+    sleep 5
+    docker exec -i $(docker ps -q --filter ancestor=bedrive) mysql -u root -ppassword -e "create database bedrive"
+    docker exec -i $(docker ps -q --filter ancestor=bedrive) mysql -u root -ppassword bedrive < ./bedrive.sql
 fi
-
-docker build -t bedrive .
-docker run -it -d -p 80:80 -p 3306:3306 -e MYSQL_ROOT_PASSWORD=password -v $(pwd)/bedrive:/var/www/localhost/htdocs/ bedrive
-sleep 5
-docker exec -i $(docker ps -q --filter ancestor=bedrive) mysql -u root -ppassword -e "create database bedrive"
-docker exec -i $(docker ps -q --filter ancestor=bedrive) mysql -u root -ppassword bedrive < ./bedrive.sql
