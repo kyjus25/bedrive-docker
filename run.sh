4342031d2636b9493fb5eacc369852f445ac2241
Justin White <kyjus25@gmail.com>
2019-12-27 20:41:00 +0000

    Custom ARM version based on glats/alpine-lamp

--- run.sh
+++ run.sh
@@ -4,7 +4,10 @@ if [ "$(docker ps -q --filter ancestor=bedrive)" != "" ]; then
 else
     docker build -t bedrive .
     docker run -it -d -p 80:80 -p 3306:3306 -e MYSQL_ROOT_PASSWORD=password -v $(pwd)/bedrive:/var/www/localhost/htdocs/ --name bedrive bedrive
-    sleep 5
+    echo "Sleeping for 15 seconds to ensure MySQL comes up"
+    sleep 15
+    echo "Creating database"
     docker exec -i $(docker ps -q --filter ancestor=bedrive) mysql -u root -ppassword -e "create database bedrive"
+    echo "Importing tables"
     docker exec -i $(docker ps -q --filter ancestor=bedrive) mysql -u root -ppassword bedrive < ./bedrive.sql
 fi
