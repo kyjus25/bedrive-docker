2e7dc2acadc734767e73bb3466644db2b5f2d9a8
Justin White <kyjus25@gmail.com>
2019-12-27 20:50:48 +0000

    unzip and sleep longer

--- run.sh
+++ run.sh
@@ -2,6 +2,7 @@ if [ "$(docker ps -q --filter ancestor=bedrive)" != "" ]; then
     docker stop $(docker ps -q --filter ancestor=bedrive)
     docker start bedrive
 else
+    unzip bedrive.zip
     docker build -t bedrive .
     docker run -it -d -p 80:80 -p 3306:3306 -e MYSQL_ROOT_PASSWORD=password -v $(pwd)/bedrive:/var/www/localhost/htdocs/ --name bedrive bedrive
     echo "Sleeping for 15 seconds to ensure MySQL comes up"
